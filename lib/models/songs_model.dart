00045f3ff2aad46a174438981041b715b6defba2
disham <disha4mourya@gmail.com>
2019-04-17 09:35:43 +0530

    showing track title in grid view[IP]

--- lib/models/songs_model.dart
+++ lib/models/songs_model.dart
@@ -1,16 +1,32 @@
-class ResultModel{
-
+class ResultModel {
   int resultCount;
-  List<SongsModel> results=[];
+  List<SongsModel> results = [];
 
-  ResultModel.fromJson(Map<String, dynamic> parsedJson){
-    resultCount=parsedJson['resultCount'];
-    print("CountIS" +resultCount.toString());
+  ResultModel.fromJson(Map<String, dynamic> parsedJson) {
+    resultCount = parsedJson['resultCount'];
+    print("CountIS" + resultCount.toString());
+
+    List<SongsModel> songs = [];
+
+    for (int i = 0; i < parsedJson['results'].length; i++) {
+      SongsModel songModel = SongsModel(parsedJson['results'][i]);
+      songs.add(songModel);
+    }
+
+    results = songs;
   }
-
 }
 
-class SongsModel{
+class SongsModel {
   String _wrapper_type;
   String _track_name;
+
+  SongsModel(result) {
+    _wrapper_type = result['wrapperType'];
+    _track_name = result['trackName'];
+  }
+
+  String get wrapper_type => _wrapper_type;
+
+  String get track_name => _track_name;
 }
--- lib/view/songs_list.dart
+++ lib/view/songs_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slice_one/bloc/songs_bloc.dart';
+import 'package:flutter_slice_one/models/songs_model.dart';
 
 class SongsList extends StatefulWidget {
   @override
@@ -21,6 +22,16 @@ class SongsListState extends State<SongsList> {
       appBar: AppBar(
         title: Text('SOngs'),
       ),
+      body: StreamBuilder(
+          stream: bloc.allSongs,
+          builder: (context, AsyncSnapshot<ResultModel> snapshot) {
+            if (snapshot.hasData) {
+              return buildList(snapshot);
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+            return Center(child: CircularProgressIndicator());
+          }),
     );
   }
 
@@ -29,4 +40,18 @@ class SongsListState extends State<SongsList> {
     bloc.dispose();
     super.dispose();
   }
+}
+
+Widget buildList(AsyncSnapshot<ResultModel> snapshot) {
+  return GridView.builder(
+      itemCount: snapshot.data.results.length,
+      gridDelegate:
+          new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+      itemBuilder: (BuildContext context, int index) {
+        return GridTile(
+            child: InkResponse(
+          enableFeedback: true,
+          child: Text(snapshot.data.results[index].track_name),
+        ));
+      });
 }
