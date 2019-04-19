40e9f9665aaa16f32d3991fbd68ceb23efd28354
disham <disha4mourya@gmail.com>
2019-04-19 12:56:42 +0530

    use separated models for server call

--- lib/songs_list.dart
+++ lib/songs_list.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_slice_one/models/songs_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:rxdart/rxdart.dart';
 
@@ -16,45 +17,6 @@ Future<ResultModel> fetchPost() async {
     // If that call was not successful, throw an error.
     throw Exception('Failed to load post');
   }
-}
-
-class ResultModel {
-  int resultCount;
-  List<SongsModel> results = [];
-
-  ResultModel.fromJson(Map<String, dynamic> parsedJson) {
-    resultCount = parsedJson['resultCount'];
-    List<SongsModel> songs = [];
-
-    for (int i = 0; i < parsedJson['results'].length; i++) {
-      SongsModel songModel = SongsModel(parsedJson['results'][i]);
-      songs.add(songModel);
-    }
-
-    results = songs;
-  }
-}
-
-class SongsModel {
-  String _wrapper_type;
-  String _track_name;
-  String artworkUrl30;
-  String artworkUrl100;
-
-  SongsModel(result) {
-    _wrapper_type = result['wrapperType'];
-    _track_name = result['trackName'];
-    artworkUrl30 = result['artworkUrl30'];
-    artworkUrl100 = result['artworkUrl100'];
-  }
-
-  String get wrapper_type => _wrapper_type;
-
-  String get track_name => _track_name;
-
-  String get artworkUrl_30 => artworkUrl30;
-
-  String get artworkUrl_100 => artworkUrl100;
 }
 
 void main() => runApp(AsyncCall());
